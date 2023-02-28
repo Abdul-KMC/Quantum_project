147be0eb957eab2a6c54c2fe7a238c778612615b
Azeem-KMC <abdazekha002@gmail.com>
2023-02-28 21:01:44 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import "./App.css";
 import LoginSignupPage from "./Pages/login";
 import RegistrationPage from "./Pages/registration";
 import Table from "./Pages/table";
+import Navbar from "./Pages/navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
     <div className="App">
-      <LoginSignupPage />
-      <RegistrationPage />
-      <Table />
+      
+      <Routes>
+            <Route exact path="/Login" element={<LoginSignupPage/>} />
+            <Route exact path="/Registration" element={<RegistrationPage/>} />
+            <Route exact path="/Table" element={<Table/>} />
+          </Routes>
     </div>
+    </BrowserRouter>
+    </>
   );
 }
 
